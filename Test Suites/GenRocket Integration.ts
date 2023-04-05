7e2a9ad430dba7104cf5197e4f45f3d13b810816
sakthi <sakthi@sakthivels-mbp>
2023-04-05 12:11:42 +0100

    commit#3

--- Test Suites/GenRocket Integration.ts
+++ Test Suites/GenRocket Integration.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>2d49809a-ed07-4088-8369-9161e41ecf53</testSuiteGuid>
+   <testCaseLink>
+      <guid>8614bfb7-8f13-4afb-be97-26d8744beb83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/TC01_with GenRocket Integration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/Without GenRocket Integration.ts
+++ Test Suites/Without GenRocket Integration.ts
@@ -5,10 +5,53 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>90026dac-cb81-47d0-8ae9-c419b6898a28</testSuiteGuid>
+   <testCaseLink>
+      <guid>7afd3fe5-d3c7-4ff6-87cf-253ec67b3a7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/TC01_without GenRocket Integration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>636486ff-369b-4b63-af8b-f97b6fb6f806</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7fd1e4f9-8c71-4e88-8113-4cb3f8242c33</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3f2d6b16-8da5-4cbc-8c47-259ed05daa24</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d53e787e-1c79-4dd9-b1e3-a352b85c7122</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>72721c32-edee-48cc-9a67-6b9e5041ca3b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>33628aff-258a-4148-b399-3150d312f575</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
